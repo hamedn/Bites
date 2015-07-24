e050d650407d888f516d1a0bc0ade47511208d0f
hamedn <hamed@alnoorgames.com>
2015-07-23 21:04:50 -0400

    Begin login screen

--- BitesApp/www/js/app.js
+++ BitesApp/www/js/app.js
@@ -31,6 +31,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   // Each state's controller can be found in controllers.js
   $stateProvider
 
+
+//----------------------LOGIN STATE
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login-screen.html'
+  })
+
+
+
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
@@ -80,6 +89,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
