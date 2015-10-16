f9395e55614e137d6f603133f3d20d77f6b558ee
hamedn <hamed@alnoorgames.com>
2015-10-16 14:22:29 -0400

    added local storage service

--- BitesApp/www/js/services.js
+++ BitesApp/www/js/services.js
@@ -8,6 +8,22 @@ angular.module('starter.services', [])
   };
 })
 
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
+}])
 
 
 .factory('Chats', function() {
