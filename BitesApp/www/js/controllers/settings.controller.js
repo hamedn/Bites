2b720c0d29c088caa43506bce39b9b961b8f0e32
hamedn <hamed@alnoorgames.com>
2016-01-01 21:41:52 -0500

    logout

--- BitesApp/www/js/controllers/settings.controller.js
+++ BitesApp/www/js/controllers/settings.controller.js
@@ -1,6 +1,6 @@
 angular.module('settings.controllers', ['ionic-ratings'])
 
-.controller("SettingsCtrl", function($scope, $rootScope, $state, $stateParams, localStorage, APIServer, $http) {
+.controller("SettingsCtrl", function($scope, $rootScope, $state, $stateParams, localStorage, APIServer, $http, $ionicPopup) {
   $scope.$on('$ionicView.enter', function(e) {
     chef = localStorage.get("isChef");
     console.log(chef);
@@ -16,8 +16,19 @@ angular.module('settings.controllers', ['ionic-ratings'])
   }
 
   $scope.logOut = function () {
-    localStorage.set("loggedIn","false");
-    $state.go("preapp.splashscreen");
+
+    var confirmPopup = $ionicPopup.confirm({
+       title: 'Confirm Logout',
+       template: 'Are you sure you want to log out?'
+     });
+     confirmPopup.then(function(res) {
+       if(res) {
+          localStorage.set("loggedIn","false");
+           $state.go("preapp.splashscreen");
+       } 
+     });
+
+   
   }
 
 })
