5a3aadc8c8ed381f2b6df8b4f39c2f7f422c1091
hamedn <hamed@alnoorgames.com>
2016-01-02 02:41:18 -0500

    added production info

--- BitesAPI/credentials.js
+++ BitesAPI/credentials.js
@@ -7,7 +7,7 @@ module.exports = {
 			connectionString: 'mongodb://BitesServer:Launch123@ds029803.mongolab.com:29803/heroku_98rtcrbj'
 		},
 		production: {
-			connectionString: 'your_connect;'
+			connectionString: 'mongodb://BitesServer:Launch123@ds029803.mongolab.com:29803/heroku_98rtcrbj'
 		}
 	},
 
@@ -19,7 +19,15 @@ module.exports = {
 				callBackURL: 'http://localhost:3000/auth/facebook/callback',
 				successURL: '/success',
 				failURL: '/fail'
+			},
+			production: {
+				appId: '1596595767272078',
+				appSecret: '57a67c051886e350c539c4cc3913ea3c',
+				callBackURL: 'http://mighty-shore-9561.herokuapp.com/auth/facebook/callback',
+				successURL: '/success',
+				failURL: '/fail'
 			}
+
 		}
 	}
 
