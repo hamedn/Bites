da658fa8b7ff4011aa20b8377af070bbae430e93
eshanagarwal <agarwal.eshan@columbia.edu>
2016-01-03 17:00:16 -0500

    Added stripe stuff

--- BitesApp/www/js/controllers/login.controller.js
+++ BitesApp/www/js/controllers/login.controller.js
@@ -10,59 +10,80 @@ angular.module('login.controllers', ['ionic-ratings'])
 .controller('LoginCtrl', function($scope, $window, $location, $http, APIServer, $state, localStorage) {
    $scope.data = {};
 
-
     document.addEventListener("deviceready", onDeviceReady, false);
     function onDeviceReady() {
         window.open = cordova.InAppBrowser.open;
     }
-
-
 
   $scope.registerLocal = function () {
 
     if ($scope.data.password == $scope.data.confirm && $scope.data.agreedToTerms == true) {
       if (typeof $scope.data.chef == "undefined")
         $scope.data.chef = false;
+
+      console.log($scope.data.cardNumber);
+      $http({
+            method: 'POST',
+            url: APIServer.url() + '/saveStripeCardDetails',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+
+            transformRequest: function(obj) {
+              var str = [];
+              for(var p in obj)
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+              return str.join("&");
+            },
+
+           data:  {
+            cardNumber: $scope.data.cardNumber,
+            cvc: $scope.data.CVC,
+            exp_month: $scope.data.exp_month,
+            exp_year: $scope.data.exp_year
+          }
+            
+        }).then (function (response) {
+          console.log(response);
+        });
       
-      console.log("isChef: " + $scope.data.chef);
-      
+      /*
+      $http({
+          method: 'POST',
+          url: APIServer.url() + '/signup',
+          headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 
-    $http({
-        method: 'POST',
-        url: APIServer.url() + '/signup',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+          transformRequest: function(obj) {
+              var str = [];
+              for(var p in obj)
+              str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+              return str.join("&");
+          },
 
-        transformRequest: function(obj) {
-            var str = [];
-            for(var p in obj)
-            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-            return str.join("&");
-        },
+          data:  {
+            name: $scope.data.realname,
+            email: $scope.data.email,
+            password: $scope.data.password,
+            isChef: $scope.data.chef,
+            chefStripeToken: localStorage.get("stripeAccessToken"),
+            customerStripeId: $scope.data.customerID
+          }
+      }).then (function (response) {
 
-        data:  {
-          name: $scope.data.realname,
-          email: $scope.data.email,
-          password: $scope.data.password,
-          isChef: $scope.data.chef
-        }
-    }).then (function (response) {
-        console.log(response.data);
-
-        if (response.data.accessToken) {
-          alert("Login Successful!!!!");
-          localStorage.set("userToken", response.data.accessToken);
-          localStorage.set("loggedIn",true);
           console.log(response.data);
-          $state.go("preapp.dashboard"); 
 
-        }
-        else {
-          alert(response.data.message);
-        }
+          if (response.data.accessToken) {
+            alert("Login Successful!!!!");
+            localStorage.set("userToken", response.data.accessToken);
+            console.log(response.data);
+            $state.go("preapp.dashboard"); 
 
-    })
+          }
+          else {
+            alert(response.data.message);
+          }
 
+      })
 
+      */
   }
 
   else if ($scope.data.agreedToTerms == false || typeof $scope.data.agreedToTerms == "undefined") {
@@ -72,8 +93,17 @@ angular.module('login.controllers', ['ionic-ratings'])
     alert("Passwords don't match!")
   }
 }
-
-
+  //adjust form stuff
+  $scope.stripeResponseHandler = function(status, response) {
+      console.log("in stripeResponseHandler");
+      if (response.error) {
+        console.log(response.error);
+      } else {
+        console.log("no response error");
+        // response contains id and bank_account, which contains additional bank account details
+        var token = response.id;
+      }
+  }
 
 
   $scope.loginLocal = function () {
@@ -139,9 +169,13 @@ angular.module('login.controllers', ['ionic-ratings'])
 
 
     url = APIServer.url() + '/auth/facebook';
+    console.log(url);
         loginWindow = $window.open(url, '_blank', 'location=no,toolbar=no,hidden=no');
 
         loginWindow.addEventListener('loadstart', function (event) {
+
+          console.log("user finished with facebook login");
+
           hasToken = event.url.indexOf('?oauth_token=');
           if(hasToken > -1) {
             token = event.url.match("oauth_token=(.*)")[1];
@@ -192,7 +226,55 @@ $http({
 
 }
 
+  $scope.linkStripeAccount = function() {
 
+        url = APIServer.url() + "/auth/stripe";
+        
+        loginWindow = $window.open(url, '_blank', 'location=no,toolbar=no,hidden=no');
+
+        loginWindow.addEventListener('loadstart', function (event) {
+
+          console.log(event.url);
+
+          hasToken = event.url.indexOf('&code=');
+          console.log("hasToken index " + hasToken);
+          if(hasToken > -1) {
+            console.log("found code");
+            code = event.url.match("code=(.*)")[1];
+            console.log(code);
+            
+            //post code to retrieve all stripe details
+            $http({
+              method: 'POST',
+              url: 'https://connect.stripe.com/oauth/token',
+              headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+
+              transformRequest: function(obj) {
+                  var str = [];
+                  for(var p in obj)
+                  str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+                  return str.join("&");
+              },
+
+              data:  {
+                client_secret: "sk_test_TGVJ5AB4dXa1eaYooQr0MTN8",
+                clientID: "ca_7VvNpW0Em2iOnDuxSOHQyygV9PvtAfCs",
+                code: code,
+                grant_type: "authorization_code"
+              }
+            }).then( function (body) {
+                /*NO ERROR CHECKING BUILT IN YET */
+                console.log("successfully posted code to stripe");
+                var accessToken = angular.fromJson(body).data.access_token;
+                console.log(accessToken);
+                localStorage.set("stripeAccessToken", accessToken);
+                loginWindow.close();
+              })
+          
+          } 
+        })
+
+  }
 
 
 })
