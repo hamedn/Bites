46c2b1cbf4c8ca0bacae74467fb7b3f909f43f22
hamedn <hamed@alnoorgames.com>
2016-01-16 03:17:25 -0500

    ideal error handling

--- BitesApp/www/js/controllers/splash.controller.js
+++ BitesApp/www/js/controllers/splash.controller.js
@@ -18,24 +18,51 @@ console.log('hello workd"')
     	var acc = localStorage.get("userToken");
 
 
-    	//alert(localStorage.get("loggedIn") + "     " + localStorage.get("userToken"))
-    	 $http.get(APIServer.url() + '/users/byToken',{headers:{'accesstoken': acc }}).then(function(resp) {
-    	 	if (resp.data && resp.data._id) {
-    	 		console.log("TRANSITION TO DASHBOARD");
-     			$state.go("preapp.dashboard");
-			   localStorage.set("oid",resp.data._id);
-			   localStorage.set("name",resp.data.name);
-			   localStorage.set("isChef",resp.data.isChef);
+    	
 
-			}
-			else {
-				"No response. Must log in again."
-			}
-			//alert(resp);
-		  $ionicLoading.hide();
+        var startTime = new Date().getTime();
+$http({
+    method: 'GET',
+    url: APIServer.url() + '/users/byToken',
+    timeout : 4000, 
+    headers:{'accesstoken': acc }
+    }).success(function(resp, status, header, config){
+        if (resp.data && resp.data._id) {
+          console.log("TRANSITION TO DASHBOARD");
+          $state.go("preapp.dashboard");
+         localStorage.set("oid",resp.data._id);
+         localStorage.set("name",resp.data.name);
+         localStorage.set("isChef",resp.data.isChef);
+
+      }
+      else {
+        "No response. Must log in again."
+      }
+      //alert(resp);
+      $ionicLoading.hide();
+
+    }).error(function(result, status, header, config){
+        var respTime = new Date().getTime() - startTime;
+        if(respTime >= config.timeout){
+            //time out handeling
+          //  alert('time out');
+            $ionicLoading.hide();
+
+        } else{
+            //other error hanndling
+            $ionicLoading.hide();
+
+        }
+    })
 
 
-		 });
+
+
+
+
+
+
+
 
     
 
